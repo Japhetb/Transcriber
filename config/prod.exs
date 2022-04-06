bacb229c9247420d879a5eed40cd68a2800c3001
japhetb <japhetbrando@gmail.com>
2022-04-06 15:21:03 +0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ import Config
 
 config :transciber, TransciberWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "shielded-shore-81963.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "evening-harbor-58012.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
